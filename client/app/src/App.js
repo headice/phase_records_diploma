22cb0085f0ba497b9889da641da947ffbb86cfcd
headice <114868841+headice@users.noreply.github.com>
2026-06-21 21:28:33 +1000

    Add Policy component to App.js

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -269,6 +269,7 @@ export default function App() {
             <ScrollToTop />
             <ScrollToHash />
             <RouteMeta />
+            <Policy />
             <AppRoutes />
           </ShopProvider>
         </AuthProvider>
